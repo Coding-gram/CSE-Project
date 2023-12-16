7860b69182742553afd55b8eefd545d637aabcd0
Coding-gram <147648109+Coding-gram@users.noreply.github.com>
2023-12-16 22:33:11 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,10 @@
+const music = new Audio("song1.mp3");
+music.play();
+ const songs = [
+    {
+         id: 1,
+         songName: `lagde lahore di<br>
+         <div class="subtitle">Guru Randhwa</div>`,
+         poster: "1.jpg"
+    }
+ ]
